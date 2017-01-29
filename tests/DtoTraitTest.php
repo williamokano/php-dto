bcac2bed6596e86e223a648af6cb856974097a92
William Okano <williamokano@gmail.com>
2017-01-29 16:35:38 -0200

    Added test for get

--- tests/DtoTraitTest.php
+++ tests/DtoTraitTest.php
@@ -40,6 +40,19 @@ class DtoTraitTest extends PHPUnit_Framework_TestCase
         $this->assertAttributeEquals([], 'dtoChangedProperties', $this->traitObject, 'dtoChangedProperties did not reseted as exptected');
     }
 
+    public function testGet()
+    {
+        $this->traitObject->fill(['d' => 4], true);
+        $this->assertObjectHasAttribute('dtoProperties', $this->traitObject, 'dtoProperties not found');
+        $this->assertAttributeEquals(['d' => 4], 'dtoProperties', $this->traitObject, 'dtoProperties did not replace the elements for the "d" property only');
+        $this->assertObjectHasAttribute('dtoChangedProperties', $this->traitObject, 'dtoChangedProperties not found');
+        $this->assertAttributeEquals([], 'dtoChangedProperties', $this->traitObject, 'dtoChangedProperties did not reseted as exptected');
+        $this->assertEquals(4, $this->traitObject->d, 'Value is not 4');
+        $this->assertEquals(4, $this->traitObject->get('d', null), 'Value is not 4');
+        $this->assertNull($this->traitObject->get('okano'), 'The returned value is not null');
+        $this->assertEquals(666, $this->traitObject->get('okano', 666), 'The returned value is not 666');
+    }
+
     private function createObjectForTrait()
     {
         $traitName = Dto::class;
